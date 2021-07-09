65c4172f4f7c3cf8c4aff168b3f97cc1ac1288f4
Ashish Kumar <ashishpurbey221806@gmail.com>
2021-07-09 21:05:40 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '@/components/Layout'
+
+export default function AboutPage() {
+  return (
+    <Layout title='About DJ Events'>
+      <h1>Yes i am aobout</h1>
+      <p>This is an App to find the latest DJ and other musical events</p>
+      <p>version:1.0.0</p>
+    </Layout>
+  )
+}
